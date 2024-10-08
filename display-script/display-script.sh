70acb256f5c5e19cc85c2becb369ac205bf15e87
eww-git <168635962+eww-git@users.noreply.github.com>
2024-10-09 02:51:21 +0700

    Update display-script.sh

--- display-script/display-script.sh
+++ display-script/display-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xrandr --output HDMI-A-0 --scale 1.2x1.2 --mode 1920x1080 --rate 60 --output eDP --off -hsync +vsync
+xrandr --output HDMI-A-0 --scale 1.2x1.2 --mode 1920x1080 --rate 60 --output eDP --off 
 
