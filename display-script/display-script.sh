f13e19be0ca16f2622ef0e9877838c998d954967
eww-git <168635962+eww-git@users.noreply.github.com>
2024-10-11 18:26:26 +0700

    Update display-script.sh

--- display-script/display-script.sh
+++ display-script/display-script.sh
@@ -2,4 +2,7 @@
 
 xrandr --output HDMI-A-0 --scale 1.2x1.2 --mode 1920x1080 --rate 60 --output eDP --off 
 ##xrandr --output eDP --scale 1.2x1.2 --mode 1920x1080 --rate 59.93
-##xrandr --output eDP-1 --auto --output DP-1 --auto --scale 2x2 --right-of eDP-1
+
+##panda
+##xrandr --output eDP --auto --output HDMI-A-0 --mode 1600x900 --scale 1.5x1.5 --rate 60 --right-of eDP
+
