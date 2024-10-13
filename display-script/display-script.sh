599c3f4e103b82eb498676b65543428b596b85d1
eww-git <168635962+eww-git@users.noreply.github.com>
2024-10-13 14:39:23 +0700

    Update display-script.sh

--- display-script/display-script.sh
+++ display-script/display-script.sh
@@ -5,4 +5,5 @@ exec xrandr xrandr --output HDMI-A-0 --set TearFree auto --scale 1.25x1.25 --mod
 ### TearFree [auto,on,off] vrr_capable [0,1]
 
 ##panda
-xrandr --output eDP --mode 1920x1080 --rate 60 --output HDMI-A-0 --mode 1600x900 --scale 1.4x1.4 --rate 60 --right-of eDP
+##xrandr --output eDP --mode 1920x1080 --rate 60 --output HDMI-A-0 --mode 1600x900 --scale 1.4x1.4 --rate 60 --right-of eDP
+xrandr --output eDP --set vrr_capable 1 --mode 1920x1080 --rate 60 --output HDMI-A-0 --set vrr_capable 1 --mode 1600x900 --scale 1.4x1.4 --rate 60 --right-of eDP
